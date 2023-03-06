2d879b3bebcc0e23f8834ef89bab3f1b2eb3dd81
Denilson Almeida <denilson.almeida@visie.com.br>
2023-03-06 16:24:58 -0300

    Modificações em nomes das patas, cap.2 completo e início do cap 3

--- PYTHON_Estudando/intro_ppgame/cap1/exec5.py
+++
@@ -1,9 +0,0 @@
-# Tuplas
-
-contato = (11, 952764841, 'nome@gmail.com')
-
-ddd, num, email = contato # Desempacotamento
-
-print(num, email, ddd)
-
-print(contato[0:2])
--- PYTHON_Estudando/intro_ppgame/cap1/exec8.py
+++
@@ -1,8 +0,0 @@
-# Operadores de Comparação
-
-# < menor, <= menor igual, > maior, >= maior igual, == igual, != diferente
-
-
-# Booleano é basicamente True False. True é 1 e False 0 (não lembro como usa mas é isso)
-
-print(1 == 1)
